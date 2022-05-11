24d29bdb0e4f384b920ab34ea90fc11f6d4614e8
LGXprod <mattghanno@gmail.com>
2022-05-12 02:53:34 +1000

    Expanded app architecture and created some basic components

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="nav-bar">
+      <div>
+        <div>
+          <h3><span>M</span><span>G</span></h3>
+          <Link to="">Features</Link>
+          <Link to="">Entire Catalogue</Link>
+          <Link to="">About Us</Link>
+        </div>
+
+        <Link to="">View Saved</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="movie-geek-info"></div>
+      <div className="movie-geek-info"></div>
+      <div className="movie-geek-info"></div>
+    </>
+  )
+}
+
+export default Home;
